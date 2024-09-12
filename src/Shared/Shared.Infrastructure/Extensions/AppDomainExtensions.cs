a07866a827770a906028cec65e7c2a7774faa867
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-09-12 13:51:40 +0200

    Add AppDomain extension methods for assembly retrieval

---
+++ src/Shared/Shared.Infrastructure/Extensions/AppDomainExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Kubis1982.Shared.Extensions
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class AppDomainExtensions
+    {
+        public static Assembly[] GetSystemAssemblies(this AppDomain currentDomain) => currentDomain.GetAssemblies().Where(n => n.FullName?.StartsWith(SystemInformation.SystemName) == true).ToArray();
+
+        public static Assembly[] GetModuleAssemblies(this AppDomain currentDomain, string moduleName)
+        {
+            return currentDomain.GetSystemAssemblies().Where(n => n.FullName?.StartsWith($"{SystemInformation.SystemName}.Modules.{moduleName}") == true).ToArray();
+        }
+
+        public static Assembly? GetModuleSharedAssembly(this AppDomain currentDomain, string moduleName)
+        {
+            return currentDomain.GetModuleAssemblies(moduleName).FirstOrDefault(n => n.FullName?.StartsWith($"{SystemInformation.SystemName}.Modules.{moduleName}.Shared") == true);
+        }
+    }
+}
