92043a3fc5dee6c1249ca5e0a5a5f1826fec814d
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-09-12 13:50:17 +0200

    Add ModuleExtensions with GetModuleName methods

---
+++ src/Shared/Shared.Domain/Extensions/ModuleExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Kubis1982.Shared.Extensions
+{
+    using System;
+
+    public static class ModuleExtensions
+    {
+        /// <summary>
+        /// Gets a module name.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetModuleName(this object value)
+            => value?.GetType().GetModuleName() ?? string.Empty;
+
+        /// <summary>
+        /// Gets a module name.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetModuleName(this Type type)
+        {
+            string namespacePart = "Modules";
+            int splitIndex = 2;
+
+            if (type?.Namespace is null)
+            {
+                return string.Empty;
+            }
+            return type.Namespace.Contains(namespacePart)
+                ? type.Namespace.Split(".")[splitIndex]
+                : string.Empty;
+        }
+    }
+}
