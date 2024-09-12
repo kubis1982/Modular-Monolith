6ddf2a884f5f648809e20ff6f5abfeb9cf9753ac
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-09-12 13:50:34 +0200

    Add IUnitOfWork interface with CommitAsync method

---
+++ src/Shared/Shared.Domain/Persistance/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace Kubis1982.Shared.Persistance
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
