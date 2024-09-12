45907c98b0e85d4f5f235fa85ef96f04ba22301e
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-09-12 13:51:58 +0200

    Add UnitOfWork and Validation pipeline behaviors

---
+++ src/Shared/Shared.Infrastructure/Pipelines/UnitOfWorkPipelineBehavior.cs
@@ -0,0 +1,21 @@
+﻿namespace Kubis1982.Shared.Pipelines
+{
+    using Kubis1982.Shared.CQRS.Commands;
+    using Kubis1982.Shared.Persistance;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class UnitOfWorkPipelineBehavior<TRequest, TResponse>(IServiceProvider serviceProvider) : PipelineBehavior<TRequest, TResponse> where TRequest : IUnitOfWorkCommand
+    {
+        public override async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+        {
+            var response = await next();
+            var unitOfWork = serviceProvider.GetRequiredKeyedService<IUnitOfWork>(request.GetModuleName());
+            await unitOfWork.CommitAsync(cancellationToken);
+            return response;
+        }
+    }
+}
---
+++ src/Shared/Shared.Infrastructure/Pipelines/ValidationPipelineBehavior.cs
@@ -0,0 +1,35 @@
+﻿namespace Kubis1982.Shared.Pipelines
+{
+    using FluentValidation;
+    using FluentValidation.Results;
+    using MediatR;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal class ValidationPipelineBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : PipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        public override Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken = default)
+        {
+            List<ValidationFailure> errors = validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (errors.Count != 0)
+            {
+                StringBuilder errorBuilder = new();
+                foreach (ValidationFailure error in errors)
+                {
+                    errorBuilder.AppendLine(error.ErrorMessage);
+                }
+                throw new ValidationException(errorBuilder.ToString(), errors);
+            }
+
+            return next();
+        }
+    }
+}
