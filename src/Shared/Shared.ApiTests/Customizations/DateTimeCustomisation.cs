cfeab9f7380c0918999906ead5c6e1d48416649a
m.swiatnicki <m.swiatnicki@7technology.pl>
2024-09-12 17:16:17 +0200

    Add Shared.ApiTests project and customizations for testing

---
+++ src/Shared/Shared.ApiTests/Customizations/DateTimeCustomisation.cs
@@ -0,0 +1,33 @@
+﻿namespace Kubis1982.Shared.Customizations
+{
+    using AutoFixture;
+    using AutoFixture.Kernel;
+    using System;
+
+    internal class DateTimeCustomisation : ICustomization {
+        public void Customize(IFixture fixture) {
+            fixture.Customizations.Add(new DateTimeGenerator());
+        }
+    }
+
+    internal class DateTimeGenerator : ISpecimenBuilder {
+        private readonly ISpecimenBuilder generator;
+
+        public DateTimeGenerator() {
+            this.generator = new RandomDateTimeSequenceGenerator();
+        }
+
+        public object Create(object request, ISpecimenContext context) {
+            var t = request as Type;
+
+            if (t == null || t != typeof(DateTime))
+                return new NoSpecimen();
+
+            var result = this.generator.Create(request, context);
+            if (result is NoSpecimen)
+                return result;
+
+            return ((DateTime)result).ToUniversalTime();
+        }
+    }
+}
