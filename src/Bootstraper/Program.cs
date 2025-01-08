31e7fdc2d29d6a2017f1f3ceb6f8d26c9ce744c4
kubis1982 <m.swiatnicki@7technology.pl>
2025-01-08 14:07:25 +0100

    Enable OpenAPI

--- src/Bootstraper/Program.cs
+++ src/Bootstraper/Program.cs
@@ -11,11 +11,21 @@ var logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddOpenApi();
+
 builder.Services.AddModular(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
 
 app.UseModular(app.Environment);
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    //app.UseSwaggerUI();
+}
 
 app.Run();
 
